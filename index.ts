efb366f0caaaf3ddd0f0498b2956873e622addbb
Dev-Abdullahsabir <dev.abdullahsabir@gmail.com>
2024-04-24 18:22:07 +0500

    completed

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency:any = {
+  USD: 1,
+  PKR: 280,
+  EUR: 0.91,
+  INR: 74.57,
+  GBP: 0.76,
+};
+
+let userAnswer = await inquirer.prompt([
+  {
+    name : "From",
+    type : "list",
+    message : "Enter from currency",
+    choices : ["USD", "PKR", "EUR", "INR", "GBP"],
+  },
+  {
+    name : "To",
+    type : "list",
+    message : "Enter from currency",
+    choices : ["USD", "PKR", "EUR", "INR", "GBP"],
+  },
+  {
+    name : "Amount",
+    message : "Enter amount",
+    type : "number",
+  },
+]);
+
+let fromAmount = currency[userAnswer.From]
+let toAmount = currency[userAnswer.To]
+let amount = userAnswer.Amount
+let baseAmount = amount / fromAmount                   
+let convertedAmount = (baseAmount*10 * toAmount*10) / 10
+console.log(Math.floor(convertedAmount))
